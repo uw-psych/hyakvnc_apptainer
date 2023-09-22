f26819da46d16dfeef919e386b93afd56a985cb1
Altan Orhon <altan@uw.edu>
2023-09-21 17:13:46 -0700

    Updated xubuntu

--- ubuntu22.04_xubuntu/start-xubuntu.sh
+++ ubuntu22.04_xubuntu/start-xubuntu.sh
@@ -1,24 +1,24 @@
+
 #!/bin/bash
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-
+PATH="$PATH:/usr/bin:/usr/sbin"
 export XKL_XMODMAP_DISABLE=1 # disable keyboard layout switching
 
-#export XDG_DATA_DIRS="/usr/share/xfce4:/usr/share/xubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop:/usr/share"
-#export XDG_CONFIG_DIRS="/etc/xdg/xdg-xubuntu:/etc/xdg"
+export XDG_DATA_DIRS="/usr/share/xfce4:/usr/share/xubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop:/usr/share"
+export XDG_CONFIG_DIRS="/etc/xdg/xdg-xubuntu:/etc/xdg"
 
-#export LANG=en_US.UTF-8
-#export GDM_LANG=en_US.UTF-8
-#export DESKTOP_SESSION=xubuntu
-#export GDMSESSION=xubuntu
-#export XDG_SESSION_DESKTOP=xubuntu
-#PATH=/usr/local/bin:/usr/bin:/usr/sbin
+export LANG=en_US.UTF-8
+export GDM_LANG=en_US.UTF-8
+export DESKTOP_SESSION=xubuntu
+export GDMSESSION=xubuntu
+export XDG_SESSION_DESKTOP=xubuntu
 
-touch $HOME/.Xresources && -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+touch $HOME/.Xresources && [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 
 # propagate to X sessions. It is important when user first
 # login, they decide on the initial xfce/xubuntu template settings.
-#dbus-update-activation-environment --verbose XDG_DATA_DIRS XDG_CONFIG_DIRS DESKTOP_SESSION GDMSESSION XDG_SESSION_DESKTOP
+dbus-update-activation-environment --verbose XDG_DATA_DIRS XDG_CONFIG_DIRS DESKTOP_SESSION GDMSESSION XDG_SESSION_DESKTOP
 
 xset s noblank &
 xset s off &
@@ -26,3 +26,9 @@ xsetroot -solid grey
 
 tigervncconfig -iconic &
 exec startxfce4
+xset s noblank &
+xset s off &
+xsetroot -solid grey
+tigervncconfig -iconic &
+startxfce4
+
